5dd4311a37a395515dfce82e8a4eba4237c44ab4
esc-gh <94961352+esc-gh@users.noreply.github.com>
2022-10-07 16:51:41 +0100

    update and create tests. Used async instead of done

--- tests/mongoose.js
+++ tests/mongoose.js
@@ -58,4 +58,55 @@ describe("Player Characters API", function() {
         })
     })
 
+    it("/create", function(){
+
+        const pc = {
+            "name": "Mad",
+            "race": "Human",
+            "class": "Fighter",
+            "level": 6
+        }
+        
+        chai.request(server).put("/mongo/create").send(pc).end( (err, res) => {
+            chai.expect(err).to.be.null;
+
+            chai.expect(res.status).to.equal(201)
+            chai.expect(res.body).to.be.not.equal({});
+
+            pc._id = res.body._id;
+            pc.__v = res.body.__v;
+            chai.expect(JSON.stringify(res.body) === JSON.stringify(pc)).to.be.true 
+        })
+    })
+
+    it("/update", async function(){
+            const body ={
+                "name": "Mad",
+                "race": "Human",
+                "class": "Fighter",
+                "level": 6
+            }
+
+            pcModel.findOne({}).then(t => {
+                chai.request(server).post(`/mongo/update/${t.id}`).send(body).then((res) => {
+                    chai.expect(res.status).to.eql(200)  
+    
+                    chai.expect(res.body).to.have.property("New");
+                    chai.expect(res.body).to.have.property("Old");
+    
+                    chai.expect(res.body.New.name).to.eql(body.name);
+                    chai.expect(res.body.Old.name).to.eql(TestPc.name);
+    
+                    chai.expect(res.body.New.race).to.eql(body.race);
+                    chai.expect(res.body.Old.race).to.eql(TestPc.race);
+    
+                    chai.expect(res.body.New.level).to.eql(res.body.Old.level);
+
+                    chai.expect(res.body.New.spells).to.eql(res.body.Old.spells);
+
+                    done()
+                })
+            })
+    })
+
 })
